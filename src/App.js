2e370837b7de0ebca8d74011ea37a17153064df1
Vinicius Teixeira Saraiva <vinicius.ts.online@gmail.com>
2023-01-16 19:50:48 -0300

    start react project

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="categories-container">
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body-container">
+          <h2>Jackets</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body-container">
+          <h2>Sneakers</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body-container">
+          <h2>Womens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body-container">
+          <h2>Mens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
